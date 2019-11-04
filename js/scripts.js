ad708ae9d84ae95a1281ef3e02a5b11e663e0d37
Student <example@example.com>
2019-11-04 14:00:49 -0800

    Create files and directories

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// var epicodusStudent = {
+//   firstName: "Charlie",
+//   lastName: "Bucket",
+//   level: 1,
+//   track: ["PHP", "Javascript", "Drupal"],
+//   enrollmentStatus: true
+// };
+//
+// console.log(epicodusStudent.level);
+//
+//
+// var myCat = {
+//   name: "Kitty Poppins",
+//   speak: function() {
+//     console.log("Meow!");
+//   }
+// };
+// myCat.name
+// "Sugar Plum"
+
+
+console.log("Hello");
